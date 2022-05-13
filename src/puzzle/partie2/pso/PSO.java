7d5c8c91715e70f38f0fb80a7382e1d97a9b7a9f
Syrina-Akai <65730591+Syrina-Akai@users.noreply.github.com>
2022-05-13 18:47:51 +0100

    pso

--- src/puzzle/partie2/pso/PSO.java
+++ src/puzzle/partie2/pso/PSO.java
@@ -1,34 +1,173 @@
 package puzzle.partie2.pso;
 
+import puzzle.partie2.pso.Particle ;
 import puzzle.Taquin;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import static puzzle.Main.idBut;
 
 public class PSO {
-    public PSO(String initID){
-        init = new Taquin(initID);
-    }
     //attributes
     private Taquin init;
-    private int gbest;
+    private double gbest;
     private Particle gbestParticle;
-    private ArrayList<Particle> particles;
+    private ArrayList<Particle> particles = new ArrayList<Particle>();
+
+    public PSO(String initID){
+        init = new Taquin(initID);
+        initialize();
+    }
+
 
     //methods
     public void PSOmain(){
-
+        //do the init idk
+        int generation = 1;
+        while(gbest != 0.0) {
+            System.out.println("generation "+ generation);
+            for (Particle element : particles) {
+                element.updateVelocity(gbest);
+                element.updatePosition();
+                element.updatePBest();
+                updateGbest();
+            }
+            updateParticles();
+            System.out.println(gbest);
+            generation++;
+        }
+        //loop through all particles
+        //calculate their velocity and position
+        //update pbest
+        //update gbest
+        //repeat until gbest = 0
     }
 
+    //create all new particles with all possible moves
     public void updateParticles(){
+        //step 1: loop through each particle
+        //step 2: retrieve the last taquin of the movelist in that particle
+        //step 3: create new particles with the new added moves
+        //step 4: add new particle to temporary arraylist
+        //step 5: once all new particles have been created, replace the particles arraylist with setter
+        ArrayList<Particle> newParticles = new ArrayList<Particle>();
+        for (Particle element: particles) {
+            ArrayList<Taquin> elemMoves = element.getMoves();
+            Taquin lastTaquin = elemMoves.get(elemMoves.size()-1);
+            Queue<Integer> nextMoves = new LinkedList<Integer>();
+            
+            //we retrieve last taquin in the particle to generate next moves
+            if (lastTaquin.getVide() % 3 != 0) {//i-1 ==> à gauche
+                nextMoves.add(lastTaquin.getVide() - 1);
+            }
+            if (lastTaquin.getVide() > 2) {//i-3 ==> en haut
+                nextMoves.add(lastTaquin.getVide() - 3);
+            }
+            if (lastTaquin.getVide() % 3 != 2) {//i+1 ==> à droite
+                nextMoves.add(lastTaquin.getVide() + 1);
+            }
+            if (lastTaquin.getVide() < 6) {//i+3 ==> en bas
+                nextMoves.add(lastTaquin.getVide() + 3);
+            }
+            //we loop through the moves to create the particles and add them
+            while(!nextMoves.isEmpty()){
+                //we get the previous move set
+                ArrayList<Taquin> addedMove = new ArrayList<>(elemMoves);
+                Taquin part = new Taquin(false);
+                //new taquin with one of the next moves
+                part.nextMove(lastTaquin, nextMoves.remove());
+                //we add that taquin to the old move set
+                addedMove.add(part);
+                //we add it to the new particle list
+                newParticles.add(new Particle(element, addedMove));
+                //System.out.println("##################################################################");
+            }
+            //System.out.println("\n###################################################################\n");
 
+        }
+        particles = newParticles;
     }
     public void updateGbest(){
+        for (Particle element : particles) {
+            if(fitness(gbestParticle.getMoves().get(gbestParticle.getMoves().size()-1).id) >= element.getPbestFit()){
+                //System.out.println("gbest");
+                //System.out.println(gbest);
+                //System.out.println("gbest fitness");
+                //System.out.println(fitness(gbestParticle.getMoves().get(gbestParticle.getMoves().size()-1).id));
+                gbest = element.getPbest();
+                gbestParticle = element;
+            }
+        }
+    }
 
+    /*public int fitness(String taquinId){
+        int fitnessVal = 0;
+        for (int i = 0 ; i < (idBut.length() > taquinId.length() ? taquinId : idBut).length() ; i++){
+            if (taquinId.charAt(i)!='0')
+                fitnessVal += idBut.charAt(i) != taquinId.charAt(i) ? 1 : 0;
+        }
+        return fitnessVal;
+    }*/
+    public int fitness(String taquinId){
+        int fitnessVal = 0;
+        int[][] matrice=new int[3][3];
+        for(int i = 0 ; i<3 ; i++){
+            for(int j = 0 ; j<3 ; j++){
+                matrice[i][j] = Integer.parseInt(taquinId.substring(i+j,i+j+1));
+            }
+        }
+        for(int i = 0 ; i<3 ; i++) {
+            for (int j = 0; j < 3; j++) {
+                fitnessVal+=(3*i+j)*matrice[i][j];
+            }
+        }
+        return fitnessVal;
     }
 
     public void initialize(){
         //create the first generation
         //init gbest
+        Queue<Integer> nextMoves = new LinkedList<>();
+        if (init.getVide() % 3 != 0) {//i-1 ==> à gauche
+            nextMoves.add(init.getVide() - 1);
+        }
+        if (init.getVide() > 2) {//i-3 ==> en haut
+            nextMoves.add(init.getVide() - 3);
+        }
+        if (init.getVide() % 3 != 2) {//i+1 ==> à droite
+            nextMoves.add(init.getVide() + 1);
+        }
+        if (init.getVide() < 6) {//i+3 ==> en bas
+            nextMoves.add(init.getVide() + 3);
+        }
+        while(!nextMoves.isEmpty()){
+            //create the first particles
+            //taquin init will be the initial taquin
+            //later taquin init will be the last of the previous moveset
+            //arraylist of taquins of moves will be an array list of one child
+            //for the update particles it will be passed on the next generation with one addition to it
+            Taquin part = new Taquin(false);
+            part.nextMove(init, nextMoves.remove());
+            ArrayList<Taquin> list = new ArrayList<Taquin>();
+            list.add(init);
+            list.add(part);
+            particles.add(new Particle(init, list));
+        }
+
+        ArrayList<Taquin> initmov = new ArrayList<Taquin>();
+        initmov.add(init);
+        gbestParticle = new Particle(init, initmov);
+        updateGbest();
+    }
+
+    public void afficherSolution(){
+        System.out.println("generated particles "+ particles.size());
+        System.out.println("size " + gbestParticle.getMoves().size());
+        for (Taquin elem: gbestParticle.getMoves()) {
+            elem.afficherTaquin();
+        }
     }
 
 }
--- src/puzzle/partie2/pso/Particle.java
+++ src/puzzle/partie2/pso/Particle.java
@@ -10,30 +10,91 @@ public class Particle {
     //attributes
     private double position;
     private double velocity;
-    private int pbest;
+    private double pbest;
     private int currentPFit;
     private int pbestFit;
     private ArrayList<Taquin> moves;
     private String binaryMoves;
-    private int decimalMoves;
+    private double decimalMoves;
+    private ArrayList<Double> butLines = new ArrayList<Double>();
+    private ArrayList<Double> butColumns = new ArrayList<Double>();
+    private double[][] matriceBut = new double[3][3];
 
     //constructor
-    public Particle(Taquin init,ArrayList<Taquin> moves, String binaryMoves){
+    public Particle(Taquin init,ArrayList<Taquin> moves){
         this.moves = moves;
         currentPFit = fitness(moves.get(moves.size() - 1).id);
         pbestFit = fitness(init.id);
-        this.binaryMoves = binaryMoves;
-        pbest = toDecimal();
+        //this.binaryMoves = binaryMoves;
         position = Math.random();
         velocity = Math.random();
+        //but values init
+
+        Taquin but = new Taquin("123804765");
+
+        for(int i = 0 ; i<3 ; i++){
+            for(int j = 0 ; j<3 ; j++){
+                matriceBut[i][j] = Double.parseDouble(but.id.substring(i+j,i+j+1));
+            }
+        }
+        //normaliser : faire la somme de chaque ligne et colonne , si somme > 1 , diviser
+        for(int i=0;i<3;i++){
+            double sum=0;
+            for(int j=0;j<3;j++){
+                sum+=matriceBut[i][j];
+            }
+            if(sum>5){
+                for(int j=0;j<3;j++){
+                    matriceBut[i][j]=matriceBut[i][j]/sum;
+                }
+            }
+        }
+        //Lines
+        for(int i = 0 ; i<3 ; i++){
+            butLines.add(matriceBut[i][0]+matriceBut[i][1]+matriceBut[i][2]);
+        }
+        //columns
+        for(int i = 0 ; i<3 ; i++){
+            butColumns.add(matriceBut[i][0]+matriceBut[i][1]+matriceBut[i][2]);
+        }
+        pbest = toDecimal();
     }
 
+    public Particle(Particle old, ArrayList<Taquin> moves){
+        pbest = old.getPbest();
+        this.moves = moves;
+        position = old.getPosition(); //takes the old position and velocity and they will be updated at the start of the pso loop
+        velocity = old.getVelocity();
+        currentPFit = fitness(moves.get(moves.size() - 1).id);
+        pbestFit = old.getPbestFit();
+        butLines = old.getButLines();
+        butColumns = old.getButColumns();
+        decimalMoves = toDecimal();
+    }
+
+
+
     //methods
-    public int fitness(String taquinId){
+    /*public int fitness(String taquinId){
         int fitnessVal = 0;
         for (int i = 0 ; i < (idBut.length() > taquinId.length() ? taquinId : idBut).length() ; i++){
             if (taquinId.charAt(i)!='0')
                 fitnessVal += idBut.charAt(i) != taquinId.charAt(i) ? 1 : 0;
+        }
+        return fitnessVal;
+    }*/
+    public int fitness(String taquinId){
+        int fitnessVal = 0;
+        int[][] matrice=new int[3][3];
+        for(int i = 0 ; i<3 ; i++){
+            for(int j = 0 ; j<3 ; j++){
+                matrice[i][j] = Integer.parseInt(taquinId.substring(i+j,i+j+1));
+            }
+        }
+        for(int i = 0 ; i<3 ; i++) {
+            for (int j = 0; j < 3; j++) {
+                fitnessVal+=(3*i+j)*matrice[i][j];
+            }
         }
         return fitnessVal;
     }
@@ -41,12 +102,12 @@ public class Particle {
     public void updatePBest(){
         //calculate the fitness and compare with pbest, replace if smaller
         currentPFit = fitness(moves.get(moves.size() - 1).id);
-        if(currentPFit > pbest){
+        if(currentPFit < pbestFit){
             pbest = decimalMoves;
         }
     }
 
-    public void updateVelocity(int gbest){
+    public void updateVelocity(Double gbest){
         double newVelo;
         double c0 = 0.7298;
         double c1 = 1.4960, c2 = 1.4960;
@@ -62,12 +123,49 @@ public class Particle {
     }
 
 
-    public String toBinary(){
+    /*public String toBinary(){
         return(Integer.toBinaryString(decimalMoves));
-    }
+    }*/
 
-    public int toDecimal(){
-        return(Integer.parseInt(binaryMoves,2));
+    public double toDecimal(){
+        double[][] matrice = new double[3][3];
+        ArrayList<Double> lines = new ArrayList<Double>();
+        ArrayList<Double> columns = new ArrayList<Double>();
+        for(int i = 0 ; i<3 ; i++){
+            for(int j = 0 ; j<3 ; j++){
+                matrice[i][j] = Double.parseDouble(moves.get(moves.size() - 1).id.substring(i+j,i+j+1));
+            }
+        }
+        //normaliser : faire la somme de chaque ligne et colonne , si somme > 1 , diviser
+        for(int i=0;i<3;i++){
+            double sum=0;
+            for(int j=0;j<3;j++){
+                sum+=matrice[i][j];
+            }
+            if(sum>5){
+                for(int j=0;j<3;j++){
+                    matrice[i][j] =matrice[i][j]/sum;
+                }
+            }
+        }
+
+        //lines
+        for(int i = 0 ; i<3 ; i++){
+            lines.add(matrice[i][0]+matrice[i][1]+matrice[i][2]);
+        }
+        //columns
+        for(int i = 0 ; i<3 ; i++){
+            columns.add(matrice[i][0]+matrice[i][1]+matrice[i][2]);
+        }
+        double differences = 0;
+
+        for (int i = 0 ; i < lines.size(); i++ ) {
+            for (int j = 0 ; j < lines.size(); j++ ){
+                differences+=Math.pow(matrice[i][j]-matriceBut[i][j],2);
+            }
+        }
+        differences=Math.sqrt(differences);
+        return(differences);
     }
 
 
@@ -88,7 +186,7 @@ public class Particle {
         this.velocity = velocity;
     }
 
-    public int getPbest() {
+    public double getPbest() {
         return pbest;
     }
 
@@ -120,12 +218,36 @@ public class Particle {
         this.binaryMoves = binaryMoves;
     }
 
-    public int getDecimalMoves() {
+    public double getDecimalMoves() {
         return decimalMoves;
     }
 
-    public void setDecimalMoves(int decimalMoves) {
+    public void setDecimalMoves(double decimalMoves) {
         this.decimalMoves = decimalMoves;
+    }
+
+    public int getPbestFit() {
+        return pbestFit;
+    }
+
+    public void setPbestFit(int pbestFit) {
+        this.pbestFit = pbestFit;
+    }
+
+    public ArrayList<Double> getButLines() {
+        return butLines;
+    }
+
+    public void setButLines(ArrayList<Double> butLines) {
+        this.butLines = butLines;
+    }
+
+    public ArrayList<Double> getButColumns() {
+        return butColumns;
+    }
+
+    public void setButColumns(ArrayList<Double> butColumns) {
+        this.butColumns = butColumns;
     }
 }
 
